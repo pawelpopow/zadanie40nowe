ea430da61b4923ac1e4bc6ece49f52d1bd29355d
ppopow <pawel.popow@gmail.com>
2020-10-19 20:57:39 +0200

    nowe zadanie40

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let paragraf=document.querySelector('p')
+paragraf.textContent = 'name';
+console.log(paragraf);
+
+function tekst (value) {
+    let plik = value;
+    var button = document.getElementById('Wyświetl dane');
+    console.log(button.value);  {
+        return value;
+    }
+
+}
